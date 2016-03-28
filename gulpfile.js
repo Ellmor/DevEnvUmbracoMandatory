70c63c2d3adb0c48c0ee6b95c0dec724ee2ae5cb
Piotr <nezi2007@gmail.com>
2016-03-29 00:07:37 +0200

    Working on ContactForm

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 ﻿var gulp = require('gulp');
 var sass = require('gulp-sass');
 gulp.task('sass', function () {
-    return gulp.src('scss/style.scss')
+    return gulp.src('scss/*.scss')
    .pipe(sass()) // Converts Sass to CSS with gulp-sass
    .pipe(gulp.dest('css/'))
-});
+});
